ba2e155cf99afbd2e1eea70f4509de11ca026198
coodori <daoh98@naver.com>
2024-04-02 00:05:24 +0900

    Solution: Stream Quiz

--- src/main/java/com/mangkyu/stream/Quiz5/Quiz5.java
+++ src/main/java/com/mangkyu/stream/Quiz5/Quiz5.java
@@ -1,7 +1,11 @@
 package com.mangkyu.stream.Quiz5;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Quiz5 {
 
@@ -9,22 +13,40 @@ public class Quiz5 {
 
     // 5.1 모든 문자열의 길이를 더한 결과를 출력하여라.
     public int quiz1() {
-        return 0;
+        return Arrays.stream(STRING_ARR)
+            .mapToInt(String::length)
+            .sum();
     }
 
     // 5.2 문자열 중에서 가장 긴 것의 길이를 출력하시오.
     public int quiz2() {
-        return 0;
+        return Arrays.stream(STRING_ARR)
+            .mapToInt(String::length)
+            .max()
+            .orElse(0);
     }
 
     // 5.3 임의의 로또번호(1~45)를 정렬해서 출력하시오.
     public List<Integer> quiz3() {
-        return Collections.emptyList();
+        return new Random().ints(1,46)
+            .distinct()
+            .limit(6)
+            .boxed()
+            .sorted()
+            .collect(Collectors.toList());
     }
 
     // 5.4 두 개의 주사위를 굴려서 나온 눈의 합이 6인 경우를 모두 출력하시오.
     public List<Integer[]> quiz4() {
-        return Collections.emptyList();
+        return IntStream.rangeClosed(1,6)
+            .boxed()
+            .flatMap(i -> IntStream.rangeClosed(1,6).boxed().map(j -> new Integer[]{i,j}))
+            .filter(arr -> arr[0] + arr[1] == 6)
+            .collect(Collectors.toList());
     }
+
+    //boxed() 메소드는 IntStream 같이 원시 타입에 대한 스트림 지원을 클래스 타입(예: IntStream -> Stream<Integer>)으로 전환해준다.
+    //
+    // 전용으로 실행 가능한 (예를 들어 int 자체로는 Collection에 못 담기 때문에 Integer 클래스로 변환하여 List<Integer> 로 담기 위해) 기능을 수행하기 위해 존재한다.
 
 }
